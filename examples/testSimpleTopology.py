96bfbdfb2e0735f088d9c461e429ab76c03901d8
Shusen Liu <liu42@llnl.gov>
2019-08-25 15:51:23 -0700

    py3 update

---
+++ examples/testSimpleTopology.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import sys
+sys.path.append('..')
+from nddavPackage import *
+
+layout = {
+    "column": [
+        # {"column": ["Neighborhood","Topological Spine"]},
+        {"row": ["Parallel Coordinate", "Neighborhood"]},
+        {"row": ["Topological Spine", 'Scatter Plot']}
+        # {"column": ["Topological Spine", "Scatter Plot"]}  # 'Scatter Plot']} "Summary P.C.", "Parallel Coordinate"
+        # {"column": ["Filtering", "Neighborhood", "Scatter Plot"]}
+    ]
+}
+
+vis = nddav(layout, port=5000)
+data = vis.addModule(DataHook)
+vis.addModule(NeighborhoodModule)
+vis.addModule(EGModule)
+
+data.loadFile("../data/terrain1.txt")
+vis.show()
