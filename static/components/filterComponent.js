9609d22aa50805682b1614d5308e4a5f51cd8a09
Shusen Liu <liu42@llnl.gov>
2019-09-04 10:22:38 -0700

    replace None with null

--- static/components/filterComponent.js
+++ static/components/filterComponent.js
@@ -1,157 +1,157 @@
 class filterComponent extends baseComponent {
-    constructor(uuid) {
-        super(uuid);
-        console.log("########## filterComponent class ###########");
-        this.addModule("DataModule");
-        this._setupUI();
+  constructor(uuid) {
+    super(uuid);
+    console.log("########## filterComponent class ###########");
+    this.addModule("DataModule");
+    this._setupUI();
 
-        this.fileToUpload = None;
+    this.fileToUpload = null;
+  }
+
+  loadFile() {
+    console.log("load file on server side\n");
+    if (this.fileToUpload) {
+      this.callModule("loadFile", {
+        "filename": "upload/" + this.fileToUpload.name
+      });
+
+      this.callModule("getModuleInfoDict", {});
     }
+  }
 
-    loadFile() {
-        console.log("load file on server side\n");
-        if (this.fileToUpload) {
-            this.callModule("loadFile", {
-                "filename": "upload/" + this.fileToUpload.name
-            });
+  sendFile(file) {
+    this.fileToUpload = file;
+    var formData = new FormData();
+    var request = new XMLHttpRequest();
+    formData.set('file', file)
+    request.open("POST", '/upload')
+    request.send(formData);
+    // request.done(()=>{this.loadFile});
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        console.log('request.readyState=', request.readyState);
+        console.log('request.status=', request.status);
+        console.log('response=', request.responseText);
 
-            this.callModule("getModuleInfoDict", {});
-        }
-    }
+        this.loadFile();
+        // var data = JSON.parse(request.responseText);
+        // var uploadResult = data['message']
+        // console.log('uploadResult=',uploadResult);
+        //
+        // if (uploadResult=='failure'){
+        //    console.log('failed to upload file');
+        //    // displayError('failed to upload');
+        // }else if (uploadResult=='success'){
+        //    console.log('successfully uploaded file');
+        //    this.loadFile();
+        // }
+      }
+    }.bind(this);
+  }
 
-    sendFile(file) {
-        this.fileToUpload = file;
-        var formData = new FormData();
-        var request = new XMLHttpRequest();
-        formData.set('file', file)
-        request.open("POST", '/upload')
-        request.send(formData);
-        // request.done(()=>{this.loadFile});
-        request.onreadystatechange=function(){
-               if (request.readyState==4 && request.status==200){
-                  console.log('request.readyState=',request.readyState);
-                  console.log('request.status=',request.status);
-                  console.log('response=',request.responseText);
+  parseFunctionReturn(msg) {
+      console.log(msg)
+      switch (msg['function']) {
+        case "getModuleInfoDict":
+          var JSONdata = msg['data'];
+          var domain = JSONdata['domain'];
+          var range = JSONdata['range'];
+          var norm = JSONdata['norm'];
+          var vars = JSONdata['vars'];
 
-                  this.loadFile();
-                  // var data = JSON.parse(request.responseText);
-                  // var uploadResult = data['message']
-                  // console.log('uploadResult=',uploadResult);
-                  //
-                  // if (uploadResult=='failure'){
-                  //    console.log('failed to upload file');
-                  //    // displayError('failed to upload');
-                  // }else if (uploadResult=='success'){
-                  //    console.log('successfully uploaded file');
-                  //    this.loadFile();
-                  // }
-               }
-            }.bind(this);
-    }
+          console.log(domain, range, vars);
+          // $(this.div).parent().css("overflow-y", "scroll");
 
-    parseFunctionReturn(msg) {
-            console.log(msg)
-            switch (msg['function']) {
-                case "getModuleInfoDict":
-                    var JSONdata = msg['data'];
-                    var domain = JSONdata['domain'];
-                    var range = JSONdata['range'];
-                    var norm = JSONdata['norm'];
-                    var vars = JSONdata['vars'];
+          var func = '[' + domain.toString() + '] -> ' + range;
+          $(this.div + 'function').val(func);
 
-                    console.log(domain, range, vars);
-                    // $(this.div).parent().css("overflow-y", "scroll");
+          $.each(vars, function(index, d) {
 
-                    var func = '[' + domain.toString() + '] -> ' + range;
-                    $(this.div + 'function').val(func);
+            $(this.div + 'function_domain').append(
+              "<input type='checkbox' value=" +
+              d +
+              " >" + d + "<br>");
+            $(this.div + 'function_range').append(
+              " <input type='radio' name='range' value=" +
+              d + " >" + d +
+              "<br>");
+          }.bind(this));
 
-                    $.each(vars, function(index, d) {
+          //add dummpy option for ignoring the function value
+          // $(this.div + 'function_range').append(
+          //     "<input type='radio' value='_'>_<br>"
+          // );
 
-                        $(this.div + 'function_domain').append(
-                            "<input type='checkbox' value=" +
-                            d +
-                            " >" + d + "<br>");
-                        $(this.div + 'function_range').append(
-                            " <input type='radio' name='range' value=" +
-                            d + " >" + d +
-                            "<br>");
-                    }.bind(this));
+          $.each(domain, function(index, d) {
+            $(this.div + "function_domain [value='" + d +
+              "']").prop(
+              'checked', true);
+          }.bind(this));
 
-                    //add dummpy option for ignoring the function value
-                    // $(this.div + 'function_range').append(
-                    //     "<input type='radio' value='_'>_<br>"
-                    // );
+          $(this.div + "function_range [value='" + range + "']").prop(
+            'checked',
+            true);
+      }
+    } //end of parseFunctionReturn()
 
-                    $.each(domain, function(index, d) {
-                        $(this.div + "function_domain [value='" + d +
-                            "']").prop(
-                            'checked', true);
-                    }.bind(this));
+  setFunction(domain, range, norm) {
+    // console.log(domain, range, norm);
+    this.callModule("setFunction", {
+      "domain": domain,
+      "range": range,
+      "norm": norm
+    });
+  }
 
-                    $(this.div + "function_range [value='" + range + "']").prop(
-                        'checked',
-                        true);
-            }
-        } //end of parseFunctionReturn()
+  _setupUI() {
+    $(this.div + 'define_function').on('click', function() {
+      $(this.div).parent().css("overflow-y", "scroll");
+      $(this.div + 'function_dialog').fadeIn(500);
+    }.bind(this));
 
-    setFunction(domain, range, norm) {
-        // console.log(domain, range, norm);
-        this.callModule("setFunction", {
-            "domain": domain,
-            "range": range,
-            "norm": norm
-        });
-    }
+    $(this.div + 'dialog_cancel').on('click', function() {
+      $(this.div).parent().css("overflow-y", "hidden");
+      $(this.div + 'function_dialog').fadeOut(500);
+    }.bind(this));
 
-    _setupUI() {
-        $(this.div + 'define_function').on('click', function() {
-            $(this.div).parent().css("overflow-y", "scroll");
-            $(this.div + 'function_dialog').fadeIn(500);
-        }.bind(this));
+    $(this.div + 'dialog_apply').on('click', function() {
+      $(this.div).parent().css("overflow-y", "hidden");
+      $(this.div + 'function_dialog').fadeOut(500);
+      this._function_change();
+    }.bind(this));
 
-        $(this.div + 'dialog_cancel').on('click', function() {
-            $(this.div).parent().css("overflow-y", "hidden");
-            $(this.div + 'function_dialog').fadeOut(500);
-        }.bind(this));
+    // upload file to server
+    $(this.div + 'file-input').on('change', function(e) {
+      this.sendFile(e.target.files[0]);
+    }.bind(this));
 
-        $(this.div + 'dialog_apply').on('click', function() {
-            $(this.div).parent().css("overflow-y", "hidden");
-            $(this.div + 'function_dialog').fadeOut(500);
-            this._function_change();
-        }.bind(this));
+    $(this.div + 'select_file').on('click', function() {
+      $(this.div + 'file-input').click();
+    }.bind(this));
 
-        // upload file to server
-        $(this.div + 'file-input').on('change', function(e) {
-            this.sendFile(e.target.files[0]);
-        }.bind(this));
+    // load file on the server
+    // $(this.div + 'load_file').on('click', function() {
+    //     this.loadFile();
+    // }.bind(this));
+  }
 
-        $(this.div + 'select_file').on('click', function() {
-            $(this.div + 'file-input').click();
-        }.bind(this));
+  //load local file
+  _loadFileToJSON(filename) {
 
-        // load file on the server
-        // $(this.div + 'load_file').on('click', function() {
-        //     this.loadFile();
-        // }.bind(this));
-    }
+  }
 
-    //load local file
-    _loadFileToJSON(filename) {
+  _function_change() {
+    var domain = []
+    $.each($(this.div + 'function_domain [type="checkbox"]:checked'),
+      function() {
+        domain.push($(this).val());
+      });
+    var range = $(this.div + 'function_range [type="radio"]:checked').val();
+    var norm = $(this.div + 'normType [type="radio"]:checked').val();
 
-    }
+    $('#function_dialog').fadeOut(500);
 
-    _function_change() {
-        var domain = []
-        $.each($(this.div + 'function_domain [type="checkbox"]:checked'),
-            function() {
-                domain.push($(this).val());
-            });
-        var range = $(this.div + 'function_range [type="radio"]:checked').val();
-        var norm = $(this.div + 'normType [type="radio"]:checked').val();
-
-        $('#function_dialog').fadeOut(500);
-
-        this.setFunction(domain, range, norm);
-    }
+    this.setFunction(domain, range, norm);
+  }
 
 } //end of class
