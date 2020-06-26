e81f1617fc702c46aa4ea851766d2497c1838e6f
edrobina <edrobina@ufl.edu>
2020-06-26 11:19:24 -0400

    will check to see if port is in use and switch to other port

--- nddav_nbextension/nddav_nbextension.py
+++ nddav_nbextension/nddav_nbextension.py
@@ -1,6 +1,7 @@
 import json
 import ipywidgets as widgets
 from traitlets import Bool, Dict, Float, Int, List, Unicode, Union
+import socket
 
 from .nddavPackage import *
 
@@ -15,6 +16,9 @@ class NDDAVDisplay(widgets.DOMWidget):
 
     _dom_element_id = Unicode(read_only=True).tag(sync=True)
     _port_number = Int().tag(sync=True)
+    _num_rows = Int().tag(sync=True)
+    _data = Int().tag(sync=True)
+    _dim = Int().tag(sync=True)
 
 def startServer(filename=None, port=5000):
     defaultLayout = {
@@ -29,11 +33,49 @@ def startServer(filename=None, port=5000):
             layout = json.load(read_file)
     else:
         layout = defaultLayout
-
+    num_rows = len(layout['column'])
     print(layout)
+
+    print(port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(('', port))
+    except:
+        s.bind(('',0))
+        port = s.getsockname()[1]
+    s.close()
+    print(port)
+
+    #global vis 
     vis = nddav(layout, port)
     vis.show()
 
     display = NDDAVDisplay()
     display._port_number = port
+    display._num_rows = num_rows
     return display
+
+def startServer2(filename=None, port=5000, data=None, dim=2):
+    layout = {
+      "column": [
+            {"row": ["Summary P.C.", "Topological Spine", "Summary Scatter"]}
+        ]
+    }
+    num_rows = len(layout['column'])
+
+    print(layout)
+    vis = nddav(layout, port)
+    vis.show()
+
+
+    print("extension module:", data)
+
+    display = NDDAVDisplay()
+    display._port_number = port
+    display._num_rows = num_rows
+    display._data = data
+    display._dim = dim
+
+    vis.updateModuleNotebook(data, dim)
+    return display
+
